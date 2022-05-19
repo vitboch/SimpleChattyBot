0c23d2694c54c24a6ac25bb2a3a93acd2688e68a
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-05-20 00:37:15 +0700

    2 part

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const input = require('sync-input')
+
 const bot = {
     name:'Aid',
     birthYear: 2022
 }
 
 console.log(`Hello! My name is ${bot.name}.
-I was created in ${bot.birthYear}.`)
+I was created in ${bot.birthYear}.
+Please, remind me your name.`)
+
+const user = input('> ')
+
+console.log(`What a great name you have, ${user}!`)
