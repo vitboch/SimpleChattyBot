44d3bced4d5eaa7e966be7009ff891a7f642b252
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-05-21 00:09:30 +0700

    3 part

--- main.js
+++ main.js
@@ -11,4 +11,14 @@ Please, remind me your name.`)
 
 const user = input('> ')
 
-console.log(`What a great name you have, ${user}!`)
+console.log(`What a great name you have, ${user}!
+Let me guess your age.
+Enter remainders of dividing your age by 3, 5 and 7.`)
+
+const remainder3 = input('> ')
+const remainder5 = input('> ')
+const remainder7 = input('> ')
+
+const age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+console.log(`Your age is ${age}; that's a good time to start programming!`)
